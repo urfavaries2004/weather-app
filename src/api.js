8bd7c8aff98773942d078ea7649c52dd07ad408a
Chandraye Saha <124530146+urfavaries2004@users.noreply.github.com>
2024-07-19 21:48:33 +0530

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,11 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "d1ae2cb316msh4ab1398ff955575p1959a5jsnc88baafff708",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "ed913a3ed96ac14e93699d8ae8bf4f85";
